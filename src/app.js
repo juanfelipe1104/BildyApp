61aeacd981bdc5e1fe5fc22993a1afaabd33a189
juanfelipe1104 <juanfelipe1104@gmail.com>
2026-03-18 20:00:44 +0100

    Añadido router
    Vacio, sin rutas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import helmet from 'helmet';
+import router from './routes/user.routes.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(helmet());
+
+app.use(router);
 
 export default app;
