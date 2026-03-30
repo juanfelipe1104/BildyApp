c9dc7392e3e6e155f343340dfbcfe1bca586e258
juanfelipe1104 <juanfelipe1104@gmail.com>
2026-03-30 17:01:08 +0200

    Mostrar virtuals al pasar a json

--- src/models/User.js
+++ src/models/User.js
@@ -73,6 +73,7 @@ const userSchema = new mongoose.Schema(
     {
         timestamps: true,
         toJSON: {
+            virtuals: true,
             transform(doc, ret) {
                 delete ret.password;
                 delete ret.verificationCode;
