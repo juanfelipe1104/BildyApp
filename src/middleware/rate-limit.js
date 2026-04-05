219cf1a9d40d7aaa724aab8cbdc0e9307f0a3502
Juan Felipe Rodriguez Cordoba <juanfelipe1104@gmail.com>
2026-04-05 20:57:06 +0200

    Aumento del rate limit

--- src/middleware/rate-limit.js
+++ src/middleware/rate-limit.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 10,
+    max: 100,
     message: {
         error: true,
         message: "Demasiadas peticiones, inténtalo más tarde"
