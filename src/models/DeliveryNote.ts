182c46cef572c94ec3030c267db66ef045c2844f
Juan Felipe Rodriguez Cordoba <juanfelipe1104@gmail.com>
2026-04-24 00:39:49 +0200

    Modelo DeliveryNote

---
+++ src/models/DeliveryNote.ts
@@ -0,0 +1,100 @@
+import mongoose, { type HydratedDocument, type Model, type Types } from 'mongoose';
+import softDeletePlugin, { type SoftDeleteFields, type SoftDeleteMethods, type SoftDeleteStatics } from '../plugins/softDelete.plugin.js';
+
+type FormatType = 'material' | 'hours';
+type Worker = {
+    name: String,
+    hour: Number
+}
+
+export interface IDeliveryNoteSign extends SoftDeleteFields {
+    signed: Boolean,
+    signedAt: Date,
+    signatureUrl: String,
+    pdfUrl: String
+}
+
+export interface IDeliveryNote extends IDeliveryNoteSign {
+    user: Types.ObjectId,
+    company: Types.ObjectId,
+    client: Types.ObjectId,
+    project: Types.ObjectId,
+    format: FormatType,
+    description: String,
+    workDate: Date,
+    material: String,
+    quantity: Number,
+    hours: Number,
+    workers: Worker[],
+    createdAt: Date,
+    updatedAt: Date
+}
+
+export type DeliveryNoteDocument = HydratedDocument<IDeliveryNote, SoftDeleteMethods>;
+export type DeliveryNoteModel = Model<IDeliveryNote, {}, SoftDeleteMethods> & SoftDeleteStatics;
+
+export const filterFields = ['name', 'cif', 'email', 'phone', 'user', 'company'];
+export const sortFields = ['name', 'cif', 'email', 'phone', 'createdAt', 'updatedAt'];
+
+const deliveryNoteSchema = new mongoose.Schema<IDeliveryNote, DeliveryNoteModel, SoftDeleteMethods>(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        company: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Company',
+            required: true
+        },
+        client: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Client',
+            required: true
+        },
+        project: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Project',
+            required: true
+        },
+        format: {
+            type: String,
+            enum: ['materials', 'hours']
+        },
+        description: {
+            type: String
+        },
+        workDate: {
+            type: Date
+        },
+        material: {
+            type: String
+        },
+        quantity: {
+            type: Number
+        },
+        hours: {
+            type: Number
+        },
+        workers: [{
+            name: {
+                type: String
+            },
+            hour: {
+                type: Number
+            }
+        }]
+    }, {
+    timestamps: true,
+    versionKey: false
+}
+)
+
+deliveryNoteSchema.index({ cif: 1 });
+
+deliveryNoteSchema.plugin(softDeletePlugin);
+
+const DeliveryNote = mongoose.model<IDeliveryNote, DeliveryNoteModel>('DeliveryNote', deliveryNoteSchema);
+
+export default DeliveryNote;
